e2f821c26fc2a983ad2cf4ed9de50b2d21ed73c8
王俊杰 <jack@retzero.com>
2020-10-31 17:25:53 +0800

    update

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 import Aria2 from 'aria2';
+import browser from 'webextension-polyfill'
 
 function validateUrl(value) {
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
@@ -51,6 +52,11 @@ function downloadAgent() {
                         dir: directory,
                         out: filename
                     };
+
+                    if(true){
+                        params.split = 64;
+                        params.header = ['User-Agent: netdisk;7.0.5.9;WindowsBaiduYunGuanJia']
+                    }
                 }
                 await aria2.call("addUri", [downloadUrl], params).then(async () => {
                     // Added successfully: Cancels and removes the download from browser download manager
